5777e8bd99945d2e63ba9a9ae2e403998122c4a8
kimsieun1124 <83447076+kimsieun1124@users.noreply.github.com>
2023-12-07 13:25:20 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n,i,cnt=0;
+    int *p;
+    scanf("%d",&n);
+    p=(int *)malloc(sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);
+        if(p[i]==2){
+            cnt++;
+            if(cnt==3) {
+                printf("%d",i+1);
+                break;
+            }
+        }
+        
+    }
+    return 0;
+}
